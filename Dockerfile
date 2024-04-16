5fc75ce45c76bd119a5105657ba832ec49850753
kozakura913 <98575220+kozakura913@users.noreply.github.com>
2024-04-16 13:04:06 +0900

    wip:17

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,15 @@ FROM --platform=$BUILDPLATFORM rust:alpine
 ARG BUILDARCH
 ARG TARGETARCH
 RUN apk add --no-cache clang musl-dev curl meson ninja pkgconfig git
+ENV PATH="/aarch64-linux-musl-cross/bin:${PATH}"
+ENV CC=aarch64-linux-musl-gcc
+ENV AR=aarch64-linux-musl-ar
+ENV PKG_CONFIG_PATH=/app/dav1d/lib/pkgconfig
+ENV LD_LIBRARY_PATH=/app/dav1d/lib
+ENV CARGO_HOME=/var/cache/cargo
+ENV SYSTEM_DEPS_LINK=static
+ENV RUSTFLAGS="-C link-args=-Wl,-lc -C linker=aarch64-linux-musl-gcc"
+ENV PKG_CONFIG_SYSROOT_DIR="/aarch64-linux-musl-cross/"
 COPY crossfiles /app/crossfiles
 RUN sh /app/crossfiles/arm64.sh
 WORKDIR /app
@@ -9,7 +18,7 @@ COPY avif-decoder_dep ./avif-decoder_dep
 COPY src ./src
 COPY Cargo.toml ./Cargo.toml
 COPY asset ./asset
-RUN --mount=type=cache,target=/var/cache/cargo cargo build --release --target ${RUST_TARGET}
+RUN --mount=type=cache,target=/var/cache/cargo cargo build --release --target aarch64-unknown-linux-musl
 
 FROM alpine:latest
 ARG UID="852"
--- crossfiles/arm64.sh
+++ crossfiles/arm64.sh
@@ -1,19 +1,9 @@
 rustup target add aarch64-unknown-linux-musl
 curl -sSL https://musl.cc/aarch64-linux-musl-cross.tgz | tar -zxf - -C /
-export PATH="/aarch64-linux-musl-cross/bin:${PATH}"
 git clone --branch 1.3.0 --depth 1 https://code.videolan.org/videolan/dav1d.git /dav1d
 cd /dav1d
-export CC=aarch64-linux-musl-gcc
-export AR=aarch64-linux-musl-ar
 meson build -Dprefix=/app/dav1d -Denable_tools=false -Denable_examples=false -Ddefault_library=static --buildtype release --cross-file=/app/crossfiles/$TARGETARCH.meson 
 ninja -C build
 ninja -C build install
-export PKG_CONFIG_PATH=/app/dav1d/lib/pkgconfig
-export LD_LIBRARY_PATH=/app/dav1d/lib
-export CARGO_HOME=/var/cache/cargo
-export SYSTEM_DEPS_LINK=static
-export RUSTFLAGS="-C link-args=-Wl,-lc -C linker=aarch64-linux-musl-gcc"
-export PKG_CONFIG_SYSROOT_DIR="/aarch64-linux-musl-cross/"
-export RUST_TARGET=aarch64-unknown-linux-musl
 cp -r /app/dav1d/lib/* /aarch64-linux-musl-cross/lib/
 cp -r /app/dav1d/include/dav1d /aarch64-linux-musl-cross/include/
