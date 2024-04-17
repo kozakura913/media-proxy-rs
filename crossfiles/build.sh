3b14761d3d2449517f3fe56e5dfd191f857284b8
kozakura913 <98575220+kozakura913@users.noreply.github.com>
2024-04-17 14:13:26 +0900

    クロスコンパイルではmoldを使わない

--- crossfiles/build.sh
+++ crossfiles/build.sh
@@ -6,7 +6,6 @@ if [ ${TARGETARCH} = ${BUILDARCH} ]; then
 	cp /app/target/release/media-proxy-rs /app/media-proxy-rs
 	exit 0
 fi
-export RUSTFLAGS=${RUSTFLAGS}" -C link-arg=-fuse-ld=mold"
 source /app/crossfiles/${TARGETARCH}.sh
 cargo build --release --target ${RUST_TARGET}
 cp /app/target/${RUST_TARGET}/release/media-proxy-rs /app/media-proxy-rs
