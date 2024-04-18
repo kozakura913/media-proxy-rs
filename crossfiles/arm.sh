bbc2bcdaf8dd0f1255e9c10651196ebe8a748e94
kozakura913 <98575220+kozakura913@users.noreply.github.com>
2024-04-18 10:55:34 +0900

    armv7試し

--- crossfiles/arm.sh
+++ crossfiles/arm.sh
@@ -4,7 +4,7 @@ export CC=armv7l-linux-musleabihf-gcc
 export CXX=armv7l-linux-musleabihf-g++
 export AR=armv7l-linux-musleabihf-ar
 export CPU_FAMILY=arm
-export TARGET_CPU=arm
+export TARGET_CPU=armv7
 export RUSTFLAGS="-C link-args=-Wl,-lc -C linker=${CC}"
 export PKG_CONFIG_SYSROOT_DIR="/${MUSL_NAME}/"
 export RUST_TARGET="armv7-unknown-linux-musleabihf"
