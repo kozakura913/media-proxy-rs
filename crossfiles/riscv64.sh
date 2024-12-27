75bb9e7dffce457c721bd3385e595eb145c9da49
kozakura913 <98575220+kozakura913@users.noreply.github.com>
2024-12-28 08:52:02 +0900

    rm

--- crossfiles/riscv64.sh
+++ crossfiles/riscv64.sh
@@ -3,6 +3,6 @@ export PATH="/musl/${MUSL_NAME}/bin:${PATH}"
 export CC=riscv64-linux-musl-gcc
 export CXX=riscv64-linux-musl-g++
 export AR=riscv64-linux-musl-ar
-export RUSTFLAGS="-C linker=${CC} -C target-feature=+crt-static"
+export RUSTFLAGS="-C linker=${CC} "
 export PKG_CONFIG_SYSROOT_DIR="/musl/${MUSL_NAME}/"
 export RUST_TARGET="riscv64gc-unknown-linux-musl"
