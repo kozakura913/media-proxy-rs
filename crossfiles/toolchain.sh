77d57408311a4186907521a85e3a7a1010c43aa2
kozakura913 <98575220+kozakura913@users.noreply.github.com>
2024-12-29 04:33:28 +0900

    add

--- crossfiles/toolchain.sh
+++ crossfiles/toolchain.sh
@@ -1,9 +1,5 @@
 set -eu
-if [ -f "/app/crossfiles/${TARGETARCH}.sh" ]; then
-	source /app/crossfiles/${TARGETARCH}.sh
-else
-	source /app/crossfiles/${TARGETARCH}/${TARGETVARIANT}.sh
-fi
+source /app/crossfiles/autoenv.sh
 echo "SET(CMAKE_SYSTEM_NAME Linux)" > /app/crossfiles/toolchain.cmake
 echo "SET(CMAKE_C_COMPILER ${PKG_CONFIG_SYSROOT_DIR}/bin/${CC})" >> /app/crossfiles/toolchain.cmake
 echo "SET(CMAKE_CXX_COMPILER ${PKG_CONFIG_SYSROOT_DIR}/bin/${CXX})" >> /app/crossfiles/toolchain.cmake
